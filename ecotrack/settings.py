09990779d087528bcf351d1b87f9b271730f0c5f
Khxnsu <thekhonsu@proton.me>
2025-01-01 05:14:24 -0600

    Switch to PostgreSQL database for production

--- ecotrack/settings.py
+++ ecotrack/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Load environment variables from .env file
 load_dotenv()
@@ -68,11 +69,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecotrack.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
