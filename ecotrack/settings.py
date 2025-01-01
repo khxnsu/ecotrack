a2f0228d298cd1803ed48b0ee9e4bfe3b25ad4e4
Khxnsu <thekhonsu@proton.me>
2025-01-01 04:32:09 -0600

    Remove email settings

--- ecotrack/settings.py
+++ ecotrack/settings.py
@@ -109,14 +109,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # CSRF settings for secure forms
 CSRF_TRUSTED_ORIGINS = ['https://ecotrack-wdoa.onrender.com']
 
-# Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-
 # Security settings for production
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
